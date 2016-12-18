8e8c6e6460be8eef84d6b68eea6f5f4cf9fec97e
mustafakhalidcs <mustafakhalidcs@gmail.com>
2016-12-18 19:37:13 +0500

    Initial commit

---
+++ public_html/index.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Volta- Old School Word Processor</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open Sans:300,300italic,400,italic,600,600italic,700,700italic,800,800italic&subset=latin,cyrillic-ext,greek-ext,greek,latin-ext,cyrillic"/>
+
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/custom.css" rel="stylesheet">
+        <link href="css/font-awesome.min.css" rel="stylesheet">
+        <script src="js/jquery-1.11.1.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <link href="css/contact-buttons.css" rel="stylesheet" type="text/css"/>
+        <script src="js/smoothscrolling.js"></script>
+    </head>
+    <body>
+         <!-- 
+                
+                =============================================
+                        Header goes here
+                =============================================
+        -->
+       <nav class="nav navbar-fixed-top" role="navigation" id="header_nav">
+            <div class="col-md-12">
+                <div class="navbar-header">
+<!--                            It will appear on smaller screen-->
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+<!--                            button ends here-->
+                </div>
+               <div class="collapse navbar-collapse  smooth-scroll" id="navbar-collapse-1">
+                <div class="col-md-4 col-sm-4 col-xs-4">
+                    <div class="col-md-offset-3"><img src="images/logo.png" alt="logo.png" width="161" height="56" class="hidden-xs"></div>
+                </div>
+                <div class="col-md-8">
+                    <div class="col-md-6">
+                        <ul class="nav navbar-nav navbar-left" id="menu">
+                            <li><a href="#features">about</a></li>
+                            <li><a href="#about-us">Product</a></li>
+                            <li><a href="#">donate</a></li>
+                            <li><a href="#">store</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 col-md-offset-3"><ul class="nav navbar-nav navbar-left" id="menu">
+                            <li><a href="#" id="signin"><span class="signin-icon"></span>Sign In</a></li></ul>
+                    </div>
+                </div>
+            </div>
+            
+       </nav>
+                <!--Header ends here
+                ==================================================
+                -----------Main Section starts here---------------
+                ==================================================
+                -->
+                <section id="main-section" class="col-md-12">
+                    <div class="col-md-offset-3 col-md-7">
+                            <div class="space-lg">
+                                <h1 class="heading">The old school word processor built for the cloud</h1>
+                            </div>
+                            <div class="space-sm">
+                                <h3 class="">Volta is 100% free.  No premium plans.  No distractions.</h3>
+                            </div>
+                            <div class="space-sm">
+                                <div class="text-center">
+                                    <p><a href="#" id="btn-write">Write Now</a></p>
+                                </div>
+                            </div>
+                    </div>
+                    <img src="images/bg-main.png"  alt="" class="img-responsive">
+                </section>
+                <section id="tag-line-section">
+                    <div class="tag-line col-md-12">
+                        <h3 class="text-center">Meet the coolest thing to happen to writing since the word processor.</h3>
+                    </div>
+                </section>
+                <section id="features">
+                    <div class="container" id="product">
+                        <div class="row">
+                            <!-- 
+                            ============================
+                                    First Row
+                            ============================ !-->
+                            <div class="col-md-6 col-xs-12">
+                                <div class="col-md-offset-3 space-lg">
+                                    <img src="images/lorem.png" alt="" width="320" height="240">
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-xs-12">
+                                <div class="space-lg">
+                                    <div class="featured-text lead">
+                                        <div class="col-md-offset-3">
+                                            <h2>Choose your terminal</h2>
+                                            Choose from a radiant selection of full-screen writing terminals meticulously styled in the fashion of first generation word processors and updated for the cloud.
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!------
+                            ===================
+                                2nd Row----
+                            ====================!-->
+                             
+                            <div class="col-md-6">
+                                <div class="space-lg">
+                                    <div class="featured-text lead">
+                                        <div class="col-md-offset-3">
+                                            <h2>Simple file management</h2>
+                                            Choose from a radiant selection of full-screen writing terminals meticulously styled in the fashion of first generation word processors and updated for the cloud.
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="col-md-offset-3 space-lg">
+                                    <img src="images/ft.png" alt="" width="320" height="240">
+                                </div>
+                            </div>
+                            
+                            <!-- 
+                            ============================
+                                    3rd Row
+                            ============================ !-->
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="col-md-6">
+                                        <div class="space-lg">
+                                            <div class="featured-text lead">
+                                                <div class="col-md-offset-3">
+                                                    <h2>Simple file management</h2>
+                                                    Imagine a dashboard in which everything was right where you would expect it. Imagine a beautiful UI that required no effort whatsoever to master. That's Volta.
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="col-md-offset-3 space-lg">
+                                            <img src="images/mouse.png" alt="" width="260" height="244">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div> <!-- End of row-->
+                        
+                    </div> <!-- End of container-->
+                </section>
+                <section id="about-us">
+                    <div class="">
+                        <h2 class="text-center" id="about-us-heading">Our  Story</h2>
+                    </div>
+                    <div class=""> 
+                        <div class="col-md-offset-1 col-md-10 col-sm-12">
+                            <p class="about-text featured-text lead">
+                               Volta is the brainchild of Writerboy: a small group of under-achieving lowlifes who are now living proof that anything is possible. Apart from our being lowlifes, the one thing we all have in common is our penchant for writing.  We just wanted a simple writing tool we would look forward to using. So we made one.
+                            </p>
+                        </div>
+                    </div>
+                </section>
+                <section id="support-volta">
+                    <div class="col-md-12">
+                        <div class="space">
+                            <h2 class="text-center">Support the Volta Project</h2>
+                        </div>
+                        <div class="space col-md-offset-1 col-md-10 col-sm-12 col-xs-12">
+                            <p class="about-text featured-text lead">Volta is currently free, and we would like to keep it that way, but our little experiment can only flourish with the support of an engaged community. So if you like what we’re doing, and you can spare the scratch, please make a donation today!</p>
+                        </div>
+                        <div class="space text-center">
+                            <p><a href="#" id="btn-donate">Donate</a></p>
+                        </div>
+                    </div>
+                </section>
+                <section id="footer-section">
+                    <div class="col-md-12">
+                         <div class="space-xs">
+                             <h3 class="text-center">CREATED BY  WRITERBOY</h3> <br>
+                        </div>
+                        <div class="text-center">
+                            <p class="footer-text">Write the one they remember you for.</p><br><br>
+                            <ul class="social-icons">
+                                <li><a href=""><img src="images/fb.png" width="48" height="48" alt=""></a></li>
+                                <li><a href=""><img src="images/tw.png" width="48" height="48" alt=""></a></li>
+                                <li><a href=""><img src="images/wd.png" width="48" height="48" alt=""></a></li>
+                                <li><a href=""><img src="images/gp.png" width="48" height="48" alt=""></a></li>
+                            </ul>
+                        </div>
+                        <div class="space-xs text-center">
+                            <p>© 2016 Writerboy, LLC.  All rights reserved. Privacy Policy</p>
+                        </div>
+                    </div>
+                </section>
+<script src="js/jquery.contact-buttons.js"></script>
+<script src="js/demo.js"></script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+
+    </body>
+</html>
+
