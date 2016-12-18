a5d09ad222148b49454b3e1956cfbe37f54f1dc7
mustafakhalidcs <mustafakhalidcs@gmail.com>
2016-12-18 19:44:48 +0500

    fixed the bugs

---
+++ public_html/inc/header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Volta- Old School Word Processor</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open Sans:300,300italic,400,italic,600,600italic,700,700italic,800,800italic&subset=latin,cyrillic-ext,greek-ext,greek,latin-ext,cyrillic"/>
+
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/custom.css" rel="stylesheet">
+        <link href="css/font-awesome.min.css" rel="stylesheet">
+        <script src="js/jquery-1.11.1.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <link href="css/contact-buttons.css" rel="stylesheet" type="text/css"/>
+        <script src="js/smoothscrolling.js"></script>
+    </head>
+    <body>
+         <!-- 
+                
+                =============================================
+                        Header goes here
+                =============================================
+        -->
+       <nav class="nav navbar-fixed-top" role="navigation" id="header_nav">
+            <div class="col-md-12">
+                <div class="navbar-header">
+<!--                            It will appear on smaller screen-->
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+<!--                            button ends here-->
+                </div>
+               <div class="collapse navbar-collapse  smooth-scroll" id="navbar-collapse-1">
+                <div class="col-md-4 col-sm-4 col-xs-4">
+                    <div class="col-md-offset-3"><img src="images/logo.png" alt="logo.png" width="161" height="56" class="hidden-xs"></div>
+                </div>
+                <div class="col-md-8">
+                    <div class="col-md-6">
+                        <ul class="nav navbar-nav navbar-left" id="menu">
+                            <li><a href="#features">about</a></li>
+                            <li><a href="#about-us">Product</a></li>
+                            <li><a href="#">donate</a></li>
+                            <li><a href="#">store</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 col-md-offset-3"><ul class="nav navbar-nav navbar-left" id="menu">
+                            <li><a href="#" id="signin"><span class="signin-icon"></span>Sign In</a></li></ul>
+                    </div>
+                </div>
+            </div>
+            
+       </nav>
+                <!--Header ends here
--- public_html/snippet.php
+++ public_html/snippet.php
@@ -1,62 +1,4 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <title>Volta- Old School Word Processor</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link href="css/custom.css" rel="stylesheet">
-        <script src="js/jquery-1.11.1.js" type="text/javascript"></script>
-        <script src="js/bootstrap.min.js" type="text/javascript"></script>
-        <script src="js/smoothscrolling.js"></script>
-    </head>
-    <body>
-         <!-- 
-                
-                =============================================
-                        Header goes here
-                =============================================
-        -->
-       <nav class="nav navbar-fixed-top" role="navigation" id="header_nav">
-            <div class="col-md-12">
-                <div class="navbar-header">
-<!--                            It will appear on smaller screen-->
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-<!--                            button ends here-->
-                </div>
-               <div class="collapse navbar-collapse  smooth-scroll" id="navbar-collapse-1">
-                <div class="col-md-4">
-                    <div class="col-md-offset-3"><img src="images/logo.png" alt="logo.png" width="161" height="56"></div>
-                </div>
-                <div class="col-md-8">
-                    <div class="col-md-6">
-                        <ul class="nav navbar-nav navbar-left" id="menu">
-                            <li><a href="#about-us">Product</a></li>
-                            <li><a href="#features">about</a></li>
-                            <li><a href="#">donate</a></li>
-                            <li><a href="#">store</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-md-3 col-md-offset-3"><ul class="nav navbar-nav navbar-left" id="menu">
-                            <li><a href="#" id="signin"><span class="signin-icon"></span>Sign In</a></li></ul>
-                    </div>
-                </div>
-            </div>
-            
-       </nav>
-                <!--Header ends here
-                ==================================================
+<?php include_once("inc/header.php"); ?>
                 -----------Main Section starts here---------------
                 ==================================================
                 -->
@@ -198,8 +140,7 @@ and open the template in the editor.
                     </div>
                 </section>
 
-    </body>
-</html>
+  <?php include_once("inc/footer.php"); ?>
 
 /* Css Starts here*/
 * {
